5871ef77a848f51d7efc981a76f0130c130cd3d2
gaia_galaxy <hitoshi.gaia.galaxy@gmail.com>
2021-12-25 09:21:01 +0900

    create and update Task

--- pages/task-page.js
+++ pages/task-page.js
@@ -4,6 +4,8 @@ import Link from "next/link";
 import Task from "../components/Task";
 import { getAllTasksData } from "../lib/tasks";
 import useSWR from "swr";
+import StateContextProvider from "../context/StateContext";
+import TaskForm from "../components/TaskForm";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 const apiUrl = `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/list-task/`;
@@ -19,29 +21,34 @@ export default function TaskPage({ staticfilteredTasks }) {
     mutate();
   }, []);
   return (
-    <Layout title="Task page">
-      <ul>
-        {filteredTasks &&
-          filteredTasks.map((task) => <Task key={task.id} task={task} taskDeleted={mutate}/>)}
-      </ul>
-      <Link href="/main-page">
-        <div className="flex cursor-pointer mt-12">
-          <svg
-            class="w-6 h-6 mr-3"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M15.707 15.707a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 010 1.414zm-6 0a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 011.414 1.414L5.414 10l4.293 4.293a1 1 0 010 1.414z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-          <span>Back to main page</span>
-        </div>
-      </Link>
-    </Layout>
+    <StateContextProvider>
+      <Layout title="Task page">
+        <TaskForm taskCreated={mutate} />
+        <ul>
+          {filteredTasks &&
+            filteredTasks.map((task) => (
+              <Task key={task.id} task={task} taskDeleted={mutate} />
+            ))}
+        </ul>
+        <Link href="/main-page">
+          <div className="flex cursor-pointer mt-12">
+            <svg
+              class="w-6 h-6 mr-3"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M15.707 15.707a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 010 1.414zm-6 0a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 011.414 1.414L5.414 10l4.293 4.293a1 1 0 010 1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <span>Back to main page</span>
+          </div>
+        </Link>
+      </Layout>{" "}
+    </StateContextProvider>
   );
 }
 export async function getStaticProps() {
